156d040271bf4962a1d67a9b27c819efaeec3ddd
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-19 14:20:52 -0700

    added sort by ID

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -79,10 +79,20 @@ const CardList = (props) => {
         setCardData(sortCards)
     }
 
+    const sortById = () => {
+        const sortCardsId = [...cardData].sort((a, b) => {
+            return a.id > b.id ? 1 : -1
+        })
+        setCardData(sortCardsId)
+    }
+
+    
+
     return (<section className='cards_container'>
     <section>
         <h2>Cards for {props.board.title}</h2>
-        <button onClick={handleSort} id="sort-a-z">Sort A-Z</button>
+            <button onClick={handleSort} id="sort-a-z">Sort A-Z</button>
+            <button onClick={sortById} >Sort By ID</button>
         <div className='card-items_container'> 
         {showCards}
         </div>
